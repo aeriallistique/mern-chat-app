02ee07961effaad0d190dd4aa3fc874bee0fe183
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2024-01-16 21:47:03 +0000

    more initial setup

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const User = require('./models/User')
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 dotenv.config();
 const mongoose = require('mongoose');
 const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+  credentials:true,
+  origin: process.env.CLIENT_URL,
+}))
 mongoose.connect(process.env.MONGO_URL)
 
 app.get('/test', (req, res)=>{
@@ -16,12 +22,17 @@ app.get('/test', (req, res)=>{
 
 app.post('/register', async(req, res)=>{
   const {username, password} = req.body;
-  const createdUser = await User.create({username, password})
- 
- jwt.sign({userId: createdUser._id}, jwtSecret, (err, token)=>{
-  if(err)throw err;
-  res.cookie('token', token).status(201).json('ok')
+  try {
+    const createdUser = await User.create({username, password})
+    jwt.sign({userId: createdUser._id}, jwtSecret,{}, (err, token)=>{
+      if(err)throw err;
+      res.cookie('token', token).status(201).json({
+        id: createdUser._id,
+      })
 })
+  } catch (error) {
+    if(error)throw error;
+  }
 })
   
 app.listen(4000)
--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import Register from "./Register";
+import { UserContextProvider } from "./UserContext";
 
 
 export function App() {
 
   axios.defaults.baseURL ='http://localhost:4000'
   axios.defaults.withCredentials = true;
+  const {username} = useContext(UserContext);
   return (
-   <Register />
+    <UserContextProvider>
+      <Register />
+    </UserContextProvider>
   )
 }
